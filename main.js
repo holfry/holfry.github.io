463b44ce3f507f37090f925704df1f88060cff99
Ulrich Hoffer <99512173+holfry@users.noreply.github.com>
2022-02-15 01:12:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function jsShowText(event) {
     var i = document.getElementById("about");
-    if (i.innerHTML === "") {
-       i.innerHTML = "Hello";
+    if (i.innerHTML = "About me & Contact") {
+       i.innerHTML = "Hey; \n I am Ulrich, student at NHCC in Brooklyn Park, MN. \n My favorite thing to do is to read online articles. Otherwise, I ride my bicycle or just get asleep. \n I am native to Africa and my family is a big part of who I am. \n I am a dedicted Student and looking to pursue my Computer Science Career!";
     }
     else {
-       i.innerHTML = "hh";
+       i.innerHTML = "About me & Contact";
     }
     event.preventDefault();
  }
